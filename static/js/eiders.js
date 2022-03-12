d0172f148cc72231818a710362f320c4f4113c89
Nicolas TANDE <nittch@linkea.org>
2022-03-12 20:04:59 +0100

    Ajout doc

--- static/js/eiders.js
+++ static/js/eiders.js
@@ -5,28 +5,36 @@ function iso8601_to_html(duration)
 
 const googleapi_key = 'AIzaSyC5UT9H9iVrP1UHv7eEcISsqc-aqjvUBuc'
 
+// callback appelé de manière asynchrone avec le modèle suivant:
+//   {
+//     title: "titre de la video"
+//     thumbnail: "url de la thumbnail par défaut"
+//     duration: durée de la vidéo au format ISO8601
+//   }
 function get_youtube_video(videoid, callback)
 {
-  $.get('https://www.googleapis.com/youtube/v3/videos', {
-    key: googleapi_key,
-    id: videoid,
-    part: 'snippet, contentDetails'
-  }, function(result) {
-    callback({
-      title: result.items[0].snippet.title,
-      thumbnail: result.items[0].snippet.thumbnails.default.url,
-      duration: result.items[0].contentDetails.duration
-    });
+  $.get('https://www.googleapis.com/youtube/v3/videos',
+    { key: googleapi_key, id: videoid, part: 'snippet, contentDetails' },
+    function(result) {
+      callback({
+        title: result.items[0].snippet.title,
+        thumbnail: result.items[0].snippet.thumbnails.default.url,
+        duration: result.items[0].contentDetails.duration
+      });
   }, 'jsonp');
 }
 
+// callback appelé de manière asynchrone avec le modèle suivant:
+//   {
+//     title: "titre de la playlist"
+//     thumbnail: "url de la thumbnail par défaut"
+//     nbvideos: nombre de vidéos dans la playlist
+//   }
 function get_youtube_playlist(playlistid, callback)
 {
-  $.get('https://www.googleapis.com/youtube/v3/playlists', {
-    key: googleapi_key,
-    id: playlistid,
-    part: 'snippet, contentDetails'
-  }, function(result) {
+  $.get('https://www.googleapis.com/youtube/v3/playlists',
+  { key: googleapi_key, id: playlistid, part: 'snippet, contentDetails' },
+  function(result) {
     callback({
       title: result.items[0].snippet.title,
       thumbnail: result.items[0].snippet.thumbnails.default.url,
@@ -35,20 +43,32 @@ function get_youtube_playlist(playlistid, callback)
   }, 'jsonp');
 }
 
+// selector: soit 'id', soit 'forUsername'.
+//   En allant sur la chaine, regarder l'url. Il y a trois formats possible
+//     https://www.youtube.com/channel/blablabla    -> utiliser 'id'
+//     https://www.youtube.com/c/blablabla -> utiliser 'id', pour récupérer le l'id afficher la source de la page et chercher 'channelId'
+//     https://www.youtube.com/user/blabla -> utiliser 'forUsername'
+// identifier: l'id vu précédemment
+// callback appelé de manière asynchrone avec le modèle suivant:
+//   {
+//     title: "titre de la chaine"
+//     thumbnail: "url de la thumbnail par défaut"
+//   }
 function get_youtube_channel(selector, identifier, callback)
 {
   let request = {
     key: googleapi_key,
     part: 'snippet'
   }
-
   request[selector] = identifier
  
-  $.get('https://www.googleapis.com/youtube/v3/channels', request, function(result) {
-    callback({
-      title: result.items[0].snippet.title,
-      thumbnail: result.items[0].snippet.thumbnails.default.url
-    });
+  $.get('https://www.googleapis.com/youtube/v3/channels',
+    request,
+    function(result) {
+      callback({
+        title: result.items[0].snippet.title,
+        thumbnail: result.items[0].snippet.thumbnails.default.url
+      });
   }, 'jsonp');
 }
 
